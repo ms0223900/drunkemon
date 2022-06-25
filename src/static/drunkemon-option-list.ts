ce213f84f94dd1569cd46792cbb5302213198d27
ms0223900 <ms0223900@gmail.com>
2022-06-25 20:42:14 +0800

    feat: add Voltorb

--- src/static/drunkemon-option-list.ts
+++ src/static/drunkemon-option-list.ts
@@ -10,6 +10,20 @@ const drunkemonOptionListData: Partial<SingleDrunkemon>[] = [
   {
     description: '酒喝越多，頭上的羽毛就會失去越多的光澤，變得越來越乾燥。',
   },
+  {
+    description: '會偷客人喝完的酒瓶，不喝酒卻總喜歡剩餘的酒香。',
+  },
+  {
+    description:
+      '會用酒桶磨平過長的門牙，非常喜歡水果酒，因此常在酒廠看見拉達，很是困擾。',
+  },
+  {
+    description:
+      '常跟小拉達一起蒐集酒瓶，一樣也不喝酒，尤其喜愛神奧地區出產酒的酒瓶。',
+  },
+  {
+    description: '如果看到大嘴雀在吵架，那麼他們一定是喝了酒在發酒瘋ㄋ。',
+  },
 ];
 
 export default drunkemonOptionListData;
